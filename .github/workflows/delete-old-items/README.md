69789175972fd925356a1feebdcfd2b6217d53d4
ilker m. sitki <ilkermeliksitki@proton.me>
2025-09-23 22:17:08 +0300

    add delete old items workflow
    this workflow deletes items in the given project that are older than
    six months and have status "done".
    
    Changes to be committed:
    	new file:   delete-old-items/README.md
    	new file:   delete-old-items/delete_old_items.sh
    	new file:   delete_old_items.yml

---
+++ .github/workflows/delete-old-items/README.md
@@ -0,0 +1,3 @@
+This scripts deletes the old items from the projects if:
+- the item has status "done"
+- the item has due date older than 6 months
---
+++ .github/workflows/delete-old-items/delete_old_items.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env bash
+
+USER="ilkermeliksitki"
+PROJECT_NUMBER=5
+LIMIT=4
+MONTHS_AGO=6
+
+# fetch project items
+
+items=$(gh project item-list "$PROJECT_NUMBER" --owner "$USER" --limit "$LIMIT" --format json --jq '.items[]')
+
+echo "$items" | jq --compact-output '.' | while read -r item; do
+    item_id=$(echo "$item" | jq --raw-output '.id')
+    item_status=$(echo "$item" | jq --raw-output '.status')
+    title=$(echo "$item" | jq --raw-output '.content.title')
+    due_date=$(echo "$item" | jq --raw-output '."due date"')
+
+    # if status is done and due date is older than $MONTHS_AGO months, delete the item from project
+    if [[ "$item_status" == "done" ]]; then
+        if [[ -n "$due_date" ]]; then
+            due_date_epoch=$(date --date="$due_date" +%s)
+            due_date_months_ago_epoch=$(date --date="$MONTHS_AGO months ago" +%s)
+            if (( due_date_epoch < due_date_months_ago_epoch )); then
+                echo "Deleting item => ${title}; with due date ${due_date}"
+            fi
+        fi
+   fi
+done
